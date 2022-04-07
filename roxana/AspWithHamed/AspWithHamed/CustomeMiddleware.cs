4cd39d916e55184e5f44a0594805f9d72e0214bf
Roxana.dehzad <Roxana.dehzad@hotmail.com>
2022-04-07 10:39:03 -0400

    HelloName

---
+++ roxana/AspWithHamed/AspWithHamed/CustomeMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspWithHamed
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            var query = httpContext.Request.Query;
+            if (query.TryGetValue("name", out var name))
+            {
+                httpContext.Response.WriteAsync($"Hello {name}");
+            }
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomeMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomeMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomeMiddleware>();
+        }
+    }
+}
